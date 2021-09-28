d765cb639b41aad7911dcb06f5530d75d698c0bc
Joaquín Capó <36108774+joakpo@users.noreply.github.com>
2021-09-28 17:44:57 +0200

    Add files via upload

---
+++ CryptoData.py
@@ -0,0 +1,43 @@
+from math import comb
+import seaborn as sns
+import pandas_datareader as web
+import matplotlib.pyplot as plt
+import mplfinance as mpf
+import datetime as dt
+
+from seaborn.palettes import dark_palette
+
+currency = "EUR"
+metric = "Close"
+
+start = dt.datetime(2018, 1, 1)
+end = dt.datetime.now()
+
+crypto = ['BTC', 'ETH', 'LTC', 'XRP' ,'DASH', 'SC']
+colnames = []
+
+first = True
+
+for ticker in crypto:
+    data = web.DataReader(f'{ticker}-{currency}', "yahoo", start, end)
+    if first:
+        combined = data[[metric]].copy()
+        colnames.append(ticker)
+        combined.columns = colnames
+        first = False
+    else:
+        combined = combined.join(data[metric])
+        colnames.append(ticker)
+        combined.columns = colnames
+
+# plt.yscale('log')
+
+for ticker in crypto:
+    plt.plot(combined[ticker], label=ticker)
+
+plt.legend(loc="upper right")
+
+combined = combined.pct_change().corr(method="pearson")
+
+sns.heatmap(combined, annot=True, cmap="coolwarm")
+plt.show()
